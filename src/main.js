c34fc84b3c481132468cb8dd0f4b5bea7238090f
Lionel Manje <manjetang@yahoo.com>
2018-10-17 21:25:15 -0700

    updated format

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDom from 'react-dom';
-import App from './components/app';
-
-const container = document.createElement('div');
-document.body.appendChild(container);
-
-ReactDom.render(<App />, container);
